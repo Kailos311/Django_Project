17eda2d22cabc855bc2085726e026b25b17ec8e0
AvramsDavis <115776806+AvramsDavis@users.noreply.github.com>
2023-01-09 21:53:44 +0400

    master

--- members/views.py
+++ members/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Member
 from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
+    member_list = Member.objects.all().values()
     template = loader.get_template('first.html')
-    return HttpResponse(template.render())
+    context = {
+        'member_list': member_list
+    }
+    return HttpResponse(template.render(context, request))
 
